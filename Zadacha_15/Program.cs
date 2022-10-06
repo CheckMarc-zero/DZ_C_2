62fb4cf2dbb2d797f27f108a68e43523af25ef3a
SandyAndy <ansigida77@gmail.com>
2022-10-06 22:27:51 +0300

    Третья задача.

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите номер дня недели от 1 до 7:");
+string n1 = Console.ReadLine();
+int n = int.Parse(n1);
+
+if (n<1 || n>7)
+{
+    Console.WriteLine("Номер введен неправильно.");
+}
+
+else
+{
+    if (n>=1 && n<=5)
+    {
+        Console.WriteLine("Это будний день.");
+    }
+
+    else
+    {
+        Console.WriteLine("Это выходной.");
+    }
+}
