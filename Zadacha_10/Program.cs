f1ae09b58c924d839f43cc5479cda9100b8425a4
SandyAndy <ansigida77@gmail.com>
2022-10-06 21:17:19 +0300

    Первая задача.

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+string n1 = Console.ReadLine();
+int n = int.Parse(n1);
+
+int x = n%100/10;
+
+if (x<0)
+{
+    x = -x;
+}
+
+Console.Write("Второй цифрой введённого числа является ");
+Console.WriteLine(x);
