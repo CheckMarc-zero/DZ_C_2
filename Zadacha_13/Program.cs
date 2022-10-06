9bd6a00edc6f05eb952621216c4f97c8978a284d
SandyAndy <ansigida77@gmail.com>
2022-10-06 22:12:43 +0300

    Вторая задача.

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число:");
+string n1 = Console.ReadLine();
+int n = int.Parse(n1);
+
+if (n<0)
+{
+    n = -n;
+}
+
+if (n/100 == 0)
+{
+   Console.WriteLine("Третьей цифры у числа нет."); 
+}
+else
+{
+while (n>1000)
+{
+    n = n/10;
+}
+
+int x = n%10;
+
+Console.Write("Третьей цифрой введённого числа является ");
+Console.WriteLine(x);
+}
